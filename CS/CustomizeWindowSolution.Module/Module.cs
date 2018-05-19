aad608b4bca6c0537bd474f9b640d2b787a21750
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-20 02:47:26 +0300

    initial commit

---
+++ CS/CustomizeWindowSolution.Module/Module.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+using DevExpress.ExpressApp;
+using System.Reflection;
+
+
+namespace CustomizeWindowSolution.Module {
+    public sealed partial class CustomizeWindowSolutionModule : ModuleBase {
+        public CustomizeWindowSolutionModule() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CS/CustomizeWindowSolution/CustomizeWindowSolution.Web/Global.asax
@@ -0,0 +1,1 @@
+<%@ Application Language="C#" CodeBehind="Global.asax.cs" Inherits="CustomizeWindowSolution.Web.Global"%>
