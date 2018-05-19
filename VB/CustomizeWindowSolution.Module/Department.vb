7c77dc97a0fc6c5b3088de667f5c42facd8a7f69
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-20 02:47:26 +0300

    initial commit

---
+++ VB/CustomizeWindowSolution.Module/Department.vb
@@ -0,0 +1,37 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Text
+Imports DevExpress.Persistent.BaseImpl
+Imports DevExpress.Persistent.Base
+Imports DevExpress.Xpo
+
+Namespace WinWebSolution.Module
+	<DefaultClassOptions, System.ComponentModel.DefaultProperty("Title"), ImageName("BO_Department")> _
+	Public Class Department
+		Inherits BaseObject
+		Private title_Renamed As String
+		Private office_Renamed As String
+		Public Sub New(ByVal session As Session)
+			MyBase.New(session)
+		End Sub
+		Public Property Title() As String
+			Get
+				Return title_Renamed
+			End Get
+			Set(ByVal value As String)
+				SetPropertyValue("Title", title_Renamed, value)
+			End Set
+		End Property
+		Public Property Office() As String
+			Get
+				Return office_Renamed
+			End Get
+			Set(ByVal value As String)
+				SetPropertyValue("Office", office_Renamed, value)
+			End Set
+		End Property
+	End Class
+
+
+End Namespace
