e53fc8a3ed09a741734bcc6dc6a42fa96c9fc2da
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-20 02:47:26 +0300

    initial commit

---
+++ VB/CustomizeWindowSolution/CustomizeWindowSolution.Web/ApplicationCode/WebApplication.vb
@@ -0,0 +1,82 @@
+Imports Microsoft.VisualBasic
+Imports DevExpress.ExpressApp
+Imports DevExpress.ExpressApp.Xpo
+Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports DevExpress.ExpressApp.Web
+
+Namespace CustomizeWindowSolution.Web
+	Partial Public Class CustomizeWindowSolutionAspNetApplication
+		Inherits WebApplication
+		Protected Overrides Sub CreateDefaultObjectSpaceProvider(ByVal args As CreateCustomObjectSpaceProviderEventArgs)
+			args.ObjectSpaceProvider = New XPObjectSpaceProviderThreadSafe(args.ConnectionString, args.Connection)
+		End Sub
+		Private module1 As DevExpress.ExpressApp.SystemModule.SystemModule
+		Private module2 As DevExpress.ExpressApp.Web.SystemModule.SystemAspNetModule
+		Private module3 As CustomizeWindowSolution.Module.CustomizeWindowSolutionModule
+		'private CustomizeWindowSolution.Module.Web.CustomizeWindowSolutionAspNetModule module4;
+		Private securityModule1 As DevExpress.ExpressApp.Security.SecurityModule
+		Private securitySimple1 As DevExpress.ExpressApp.Security.SecuritySimple
+		Private module6 As DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule
+		Private sqlConnection1 As System.Data.SqlClient.SqlConnection
+		Private authenticationActiveDirectory1 As DevExpress.ExpressApp.Security.AuthenticationActiveDirectory
+		Private module5 As DevExpress.ExpressApp.Validation.ValidationModule
+
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub CustomizeWindowSolutionAspNetApplication_DatabaseVersionMismatch(ByVal sender As Object, ByVal e As DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs) Handles MyBase.DatabaseVersionMismatch
+			e.Updater.Update()
+			e.Handled = True
+		End Sub
+
+		Private Sub InitializeComponent()
+			Me.module1 = New DevExpress.ExpressApp.SystemModule.SystemModule()
+			Me.module2 = New DevExpress.ExpressApp.Web.SystemModule.SystemAspNetModule()
+			Me.module3 = New CustomizeWindowSolution.Module.CustomizeWindowSolutionModule()
+			Me.module5 = New DevExpress.ExpressApp.Validation.ValidationModule()
+			Me.module6 = New DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule()
+			Me.securityModule1 = New DevExpress.ExpressApp.Security.SecurityModule()
+			Me.securitySimple1 = New DevExpress.ExpressApp.Security.SecuritySimple()
+			Me.sqlConnection1 = New System.Data.SqlClient.SqlConnection()
+			Me.authenticationActiveDirectory1 = New DevExpress.ExpressApp.Security.AuthenticationActiveDirectory()
+			CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+			' 
+			' module5
+			' 
+			Me.module5.AllowValidationDetailsAccess = True
+			' 
+			' securitySimple1
+			' 
+			Me.securitySimple1.Authentication = Me.authenticationActiveDirectory1
+			Me.securitySimple1.UserType = GetType(DevExpress.Persistent.BaseImpl.SimpleUser)
+			' 
+			' sqlConnection1
+			' 
+			Me.sqlConnection1.ConnectionString = "Data Source=(local);Initial Catalog=CustomizeWindowSolution;Integrated Security=S" & "SPI;Pooling=false"
+			Me.sqlConnection1.FireInfoMessageEventOnUserErrors = False
+			' 
+			' authenticationActiveDirectory1
+			' 
+			Me.authenticationActiveDirectory1.CreateUserAutomatically = True
+			Me.authenticationActiveDirectory1.LogonParametersType = Nothing
+			' 
+			' CustomizeWindowSolutionAspNetApplication
+			' 
+			Me.ApplicationName = "CustomizeWindowSolution"
+			Me.Connection = Me.sqlConnection1
+			Me.Modules.Add(Me.module1)
+			Me.Modules.Add(Me.module2)
+			Me.Modules.Add(Me.module6)
+			Me.Modules.Add(Me.module3)
+			Me.Modules.Add(Me.module5)
+			Me.Modules.Add(Me.securityModule1)
+			Me.Security = Me.securitySimple1
+'			Me.DatabaseVersionMismatch += New System.EventHandler(Of DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs)(Me.CustomizeWindowSolutionAspNetApplication_DatabaseVersionMismatch);
+			CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
+
+		End Sub
+	End Class
+End Namespace
