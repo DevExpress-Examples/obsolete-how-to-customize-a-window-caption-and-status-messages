0443e4a30873cf9a89db530f09590f68eaa22c72
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-20 02:47:26 +0300

    initial commit

---
+++ CS/CustomizeWindowSolution/CustomizeWindowSolution.Web/ApplicationCode/WebApplication.cs
@@ -0,0 +1,81 @@
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Xpo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using DevExpress.ExpressApp.Web;
+
+namespace CustomizeWindowSolution.Web {
+    public partial class CustomizeWindowSolutionAspNetApplication : WebApplication {
+        protected override void CreateDefaultObjectSpaceProvider(CreateCustomObjectSpaceProviderEventArgs args) {
+            args.ObjectSpaceProvider = new XPObjectSpaceProviderThreadSafe(args.ConnectionString, args.Connection);
+        }
+        private DevExpress.ExpressApp.SystemModule.SystemModule module1;
+        private DevExpress.ExpressApp.Web.SystemModule.SystemAspNetModule module2;
+        private CustomizeWindowSolution.Module.CustomizeWindowSolutionModule module3;
+        //private CustomizeWindowSolution.Module.Web.CustomizeWindowSolutionAspNetModule module4;
+        private DevExpress.ExpressApp.Security.SecurityModule securityModule1;
+        private DevExpress.ExpressApp.Security.SecuritySimple securitySimple1;
+        private DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule module6;
+        private System.Data.SqlClient.SqlConnection sqlConnection1;
+        private DevExpress.ExpressApp.Security.AuthenticationActiveDirectory authenticationActiveDirectory1;
+        private DevExpress.ExpressApp.Validation.ValidationModule module5;
+
+        public CustomizeWindowSolutionAspNetApplication() {
+            InitializeComponent();
+        }
+
+        private void CustomizeWindowSolutionAspNetApplication_DatabaseVersionMismatch(object sender, DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs e) {
+			e.Updater.Update();
+			e.Handled = true;
+        }
+
+        private void InitializeComponent() {
+            this.module1 = new DevExpress.ExpressApp.SystemModule.SystemModule();
+            this.module2 = new DevExpress.ExpressApp.Web.SystemModule.SystemAspNetModule();
+            this.module3 = new CustomizeWindowSolution.Module.CustomizeWindowSolutionModule();
+            this.module5 = new DevExpress.ExpressApp.Validation.ValidationModule();
+            this.module6 = new DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule();
+            this.securityModule1 = new DevExpress.ExpressApp.Security.SecurityModule();
+            this.securitySimple1 = new DevExpress.ExpressApp.Security.SecuritySimple();
+            this.sqlConnection1 = new System.Data.SqlClient.SqlConnection();
+            this.authenticationActiveDirectory1 = new DevExpress.ExpressApp.Security.AuthenticationActiveDirectory();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            // 
+            // module5
+            // 
+            this.module5.AllowValidationDetailsAccess = true;
+            // 
+            // securitySimple1
+            // 
+            this.securitySimple1.Authentication = this.authenticationActiveDirectory1;
+            this.securitySimple1.UserType = typeof(DevExpress.Persistent.BaseImpl.SimpleUser);
+            // 
+            // sqlConnection1
+            // 
+            this.sqlConnection1.ConnectionString = "Data Source=(local);Initial Catalog=CustomizeWindowSolution;Integrated Security=S" +
+                "SPI;Pooling=false";
+            this.sqlConnection1.FireInfoMessageEventOnUserErrors = false;
+            // 
+            // authenticationActiveDirectory1
+            // 
+            this.authenticationActiveDirectory1.CreateUserAutomatically = true;
+            this.authenticationActiveDirectory1.LogonParametersType = null;
+            // 
+            // CustomizeWindowSolutionAspNetApplication
+            // 
+            this.ApplicationName = "CustomizeWindowSolution";
+            this.Connection = this.sqlConnection1;
+            this.Modules.Add(this.module1);
+            this.Modules.Add(this.module2);
+            this.Modules.Add(this.module6);
+            this.Modules.Add(this.module3);
+            this.Modules.Add(this.module5);
+            this.Modules.Add(this.securityModule1);
+            this.Security = this.securitySimple1;
+            this.DatabaseVersionMismatch += new System.EventHandler<DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs>(this.CustomizeWindowSolutionAspNetApplication_DatabaseVersionMismatch);
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,9 @@
+# OBSOLETE - How to: Customize a Window Caption and Status Messages
+
+
+<p>This example is now obsolete. The most up-to-date approach is available in the <a href="http://documentation.devexpress.com/#Xaf/CustomDocument3252">How to: Customize a Window Caption</a> and <a href="http://documentation.devexpress.com/#Xaf/CustomDocument3253">How to: Customize Window Status Messages</a> help topics.</p>
+<p><br>See also:<br><a href="https://www.devexpress.com/Support/Center/p/Q471592">How to add items with images to the Status Bar (WinForms)</a><br><br></p>
+
+<br/>
+
+
